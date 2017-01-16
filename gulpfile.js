a47f328422cb1359680d15156fa43c210be7d7c4
Nils W <Nils W>
2017-01-16 09:46:09 +0100

    added gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,14 @@ gulp.task('deploy-dev', [], function () {
 
   return gulp.src(src)
     .pipe(changed(dest))
-    .dest(dest)
+    .pipe(gulp.dest(dest))
 })
+
+gulp.task('watch-dev', ['deploy-dev'], function () {
+  let watcher = gulp.watch('main/**', 'deploy-dev')
+  watcher.on('changed', function (event) {
+    console.log('File ' + event.path + ' was ' + event.type)
+  })
+})
+
+glup.task('default', ['watch-dev'])
