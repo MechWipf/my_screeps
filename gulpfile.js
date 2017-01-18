65f64910aac13153a3c819a94c0f086c388e1119
Nils W <Nils W>
2017-01-18 16:05:08 +0100

    i did things

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,22 @@
 'use strict'
 
-const gulp = require('gulp')
-const changed = require('gulp-changed')
+let gulp = require('gulp')
+let webpack = require('gulp-webpack')
+let screeps = require('gulp-screeps')
 
-gulp.task('deploy-dev', [], function () {
-  let src = 'main/**/*.js'
-  let dest = 'C:/Users/mechwipf/AppData/Local/Screeps/scripts/screeps.com/dev'
-
-  return gulp.src(src)
-    .pipe(changed(dest))
-    .pipe(gulp.dest(dest))
+gulp.task('clean', () => {
+    return true
 })
 
-gulp.task('deploy-local', [], function () {
-  let src = 'main/**/*.js'
-  let dest = 'C:/Users/mechwipf/AppData/Local/Screeps/scripts/127_0_0_1___21025/default'
+gulp.task('build', gulp.series(() => {
+    return gulp.src('src')
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('dist'))
+}))
 
-  return gulp.src(src)
-    .pipe(changed(dest))
-    .pipe(gulp.dest(dest))
+gulp.task('upload', () => {
+    return gulp.src('/dist/*.js')
+        .pipe(screeps(require('./creds')))
 })
 
-gulp.task('watch-local', ['deploy-local'], function () {
-  let watcher = gulp.watch('main/**', ['deploy-local'])
-  watcher.on('changed', function (event) {
-    console.log('File ' + event.path + ' was ' + event.type)
-  })
-})
-
-gulp.task('watch-dev', ['deploy-dev'], function () {
-  let watcher = gulp.watch('main/**', ['deploy-dev'])
-  watcher.on('changed', function (event) {
-    console.log('File ' + event.path + ' was ' + event.type)
-  })
-})
-
-gulp.task('default', ['watch-dev'])
+gulp.task('default', gulp.series(['build', 'upload']))
