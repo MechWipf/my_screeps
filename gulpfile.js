2a76c5dbc28704df45d167adedc714e14421053b
Nils W <Nils W>
2017-01-25 12:50:34 +0100

    setting names

--- gulpfile.js
+++ gulpfile.js
@@ -13,25 +13,25 @@ gulp.task('clean', () => {
 
 gulp.task('build', gulp.series(
     // Cleanup
-    () => {
+    function preBuildClean() {
         return gulp.src('dist/**/*')
             .pipe(clean())
     },
     // Transcribe the app
-    () => {
+    function transcribe() {
         return gulp.src('src')
             .pipe(webpack(require('./webpack.config.js')))
             .pipe(gulp.dest('dist'))
     },
     // Patch the mapfile so screeps can read it
-    () => {
+    function pathSourceMap() {
         return gulp.src('dist/map.json')
             .pipe(insert.prepend('module.exports='))
             .pipe(rename({ extname: '.js' }))
             .pipe(gulp.dest('dist'))
     },
     // And cleanup the json file, since screeps can not read that
-    () => {
+    function cleanSourceMap() {
         return gulp.src('dist/map.json')
             .pipe(clean())
     }
